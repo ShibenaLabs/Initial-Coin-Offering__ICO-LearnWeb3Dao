c63314efbcda78eddd0b0355d39ad2fbd525db3b
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-26 19:15:40 +0100

    wrote the deploy function to deploy the smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,13 @@
+const { ethers } = require("hardhat");
+const {crytoDevGoerliAddress,crytoDevPolygonAddress} = require("../constants")
 
 
 async function main() {
-  
+   const CryptoDevFactory = await ethers.getContractFactory("CryptoDevToken")
+   const CrytoDev = await CryptoDevFactory.deploy(crytoDevGoerliAddress)
+   await CrytoDev.deployed()
+   console.log(`The contract has been deployed`)
+   console.log(`  The contract address: ${CrytoDev.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
