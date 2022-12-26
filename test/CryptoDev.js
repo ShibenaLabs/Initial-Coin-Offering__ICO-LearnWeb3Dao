23b9c7e1a9195c4e0597704cb3363b7d1b7e2893
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-26 19:23:00 +0100

    modified the content of the file

--- test/CryptoDev.js
+++ test/CryptoDev.js
@@ -1,5 +1,9 @@
+const {ethers} = require("hardhat")
+const {main} = require("../scripts/deploy")
 describe("CrtptoDev",()=>{
+     let CryptoDev,deployer,user1,user2,user3 
     beforeEach(async()=>{
-        
+       CryptoDev = await main();
+       [deployer,user1,user2,user3] = await ethers.getSigners()
     })
 })
