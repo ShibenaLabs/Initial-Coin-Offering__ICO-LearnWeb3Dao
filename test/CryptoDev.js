543c658f1267aff4cbd41bc4c9ee79ac29fdc888
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-26 19:57:09 +0100

    wrote the second test

--- test/CryptoDev.js
+++ test/CryptoDev.js
@@ -1,9 +1,24 @@
 const {ethers} = require("hardhat")
+const {BigNumber} = ethers
 const {main} = require("../scripts/deploy")
+const { assert, expect } = require("chai")
 describe("CrtptoDev",()=>{
-     let CryptoDev,deployer,user1,user2,user3 
+     let CryptoDev,deployer,user1,user2,user3,tokenContractPrice
     beforeEach(async()=>{
+      tokenContractPrice = ethers.utils.parseEther("0.001")
        CryptoDev = await main();
        [deployer,user1,user2,user3] = await ethers.getSigners()
+    })
+    
+    it("ensures the name and symbol are correct",async()=>{
+          const name = await CryptoDev.name()
+          const symbol = await CryptoDev.symbol()
+          assert.equal(name.toString(),"Crypto Dev Token")
+          assert.equal(symbol.toString(),"CD")
+    })
+
+    it("ensures the tokenPrice is correctly set",async()=>{
+         const tokenPrice = await CryptoDev.tokenPrice()
+         expect(tokenPrice.toString()).to.equal(tokenContractPrice.toString())
     })
 })
