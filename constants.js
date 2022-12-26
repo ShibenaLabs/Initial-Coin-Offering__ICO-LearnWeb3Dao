64675bbddafc760bf6e28f6f5fdbbf684bcaa5d1
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-26 18:29:54 +0100

    added the values needed to deploy the contract

---
+++ constants.js
@@ -0,0 +1,7 @@
+const crytoDevGoerliAddress = "0x9d6510Ba85F8587e39b71FFE197c2093e9cBcB05";
+const crytoDevPolygonAddress = "0x61DC32BF84A97fC259C6B5997D2efA602CCdc745";
+
+module.exports = {
+  crytoDevGoerliAddress,
+  crytoDevPolygonAddress,
+};
