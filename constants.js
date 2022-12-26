88ea79f66932ebe8d2112ef46f1c7a67dd9b252b
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-26 19:24:11 +0100

    added the deployed contract address for testnet i.e mumbai and polygon

--- constants.js
+++ constants.js
@@ -1,7 +1,11 @@
 const crytoDevGoerliAddress = "0x9d6510Ba85F8587e39b71FFE197c2093e9cBcB05";
 const crytoDevPolygonAddress = "0x61DC32BF84A97fC259C6B5997D2efA602CCdc745";
+const goerliIcoAddress ="0x4B8E9c3bc71868311059342AFdA1bF1105160840"
+const polygonIcoAddress ="0xE7c8F6e301d705166ed99b748bB2A826f1Fc0FFC"
 
 module.exports = {
   crytoDevGoerliAddress,
   crytoDevPolygonAddress,
+  goerliIcoAddress,
+  polygonIcoAddress
 };
