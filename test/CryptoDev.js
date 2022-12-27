c9246e2d195bb75e59199c5c2ea6749df33739c8
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-27 06:04:31 +0100

    added some set of test

--- test/CryptoDev.js
+++ test/CryptoDev.js
@@ -1,5 +1,5 @@
 const { ethers } = require("hardhat")
-const { BigNumber } = ethers
+const { BigNumber,utils } = ethers
 const { main } = require("../scripts/deploy")
 const { assert, expect } = require("chai")
 describe("CrtptoDev", () => {
@@ -12,12 +12,16 @@ describe("CrtptoDev", () => {
     maxTotalSupply,
     tokensPerNFT,
     number,
-    maxValue
+    maxValue,
+    fakeprice,
+    correctPrice
   beforeEach(async () => {
     tokenContractPrice = ethers.utils.parseEther("0.001")
     maxValue = BigNumber.from("10").pow("18")
     maxTotalSupplyFromContract = BigNumber.from("10000").mul(maxValue).toString()
     tokensPerNFT = BigNumber.from("10").pow("19")
+    price = utils.parseEther("0.001")
+    correctPrice = utils.parseEther("100")
     CryptoDev = await main()
     ;[deployer, user1, user2, user3] = await ethers.getSigners()
   })
@@ -38,8 +42,15 @@ describe("CrtptoDev", () => {
     assert.equal(maxTotalSupply.toString(),maxTotalSupplyFromContract)
   })
 
-  it("set the initail state of the mapping",async()=>{
+  it("set the initial state of the mapping",async()=>{
         const tokenPrice = await CryptoDev.tokenIdsClaimed("0")
         assert.equal(tokenPrice,false)
+  })
+  
+  it("reverts if the mint fee is lesser than the price of the token specified",async()=>{
+       await expect(CryptoDev.mint(10,{value:fakeprice})).to.be.revertedWith("Ether sent is incorrect")
+  })
+  it("reverts if the totalsupply is greater than max supply of the token",async()=>{
+      await expect(CryptoDev.mint(100000,{value:correctPrice})).to.be.revertedWith( "Exceeds the max total supply available.")
   })
 })
